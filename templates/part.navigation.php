abc7c030b786628489860eac6f33c79171b09175
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2017-01-18 21:59:52 +0100

    Contact fix
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- templates/part.navigation.php
+++ templates/part.navigation.php
@@ -5,7 +5,7 @@
  * This file is licensed under the Affero General Public License version 3 or
  * later. See the COPYING file.
  *
- * @author NOIJN <fremulon@protonmail.com>
+ * @author NOIJN <skjnldsv@protonmail.com>
  * @copyright NOIJN 2015
  */
 ?>
